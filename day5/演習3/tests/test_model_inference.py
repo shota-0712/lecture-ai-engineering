8c6048e2d26d8ed167aac37f39ebf59e3b54d01e
shota-0712 <satellite0712@gmail.com>
2025-05-21 15:45:23 +0900

    blackでtest_model_inference.pyをフォーマット

--- day5/演習3/tests/test_model_inference.py
+++ day5/演習3/tests/test_model_inference.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
+
 def load_model():
     """モデルをロードする関数"""
     # day5/演習1 のモデルを使用
@@ -15,27 +16,29 @@ def load_model():
         model = pickle.load(f)
     return model
 
+
 def load_test_data():
     """テストデータをロードする関数"""
     # day5/演習1 のデータを使用
     data_path = os.path.join("day5", "演習1", "data", "test.csv")
-    
+
     # データを読み込む
     df = pd.read_csv(data_path)
-    
+
     # 前処理
-    if 'Survived' in df.columns:
-        X = df.drop(['Survived'], axis=1)
-        y = df['Survived']
+    if "Survived" in df.columns:
+        X = df.drop(["Survived"], axis=1)
+        y = df["Survived"]
     else:
         X = df
         # ダミーのラベル
         y = pd.Series([0, 1] * (len(df) // 2) + [0] * (len(df) % 2))
-    
+
     # カテゴリカル変数の処理
     X = pd.get_dummies(X)
-    
+
     return X, y
+
 
 def test_model_inference_accuracy():
     """モデルの推論精度をテスト"""
